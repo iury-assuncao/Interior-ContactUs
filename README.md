a4d1d15951fa30b5aeef3892be51329759cc6d44
Iury Assunção <iuryifba@gmail.com>
2021-11-02 11:21:22 -0300

    subindo projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+## Descrição da atividade
+
+O projeto Interior tem por objetivo explorar uma das funcionalidades mais comuns na web, o envio de formulários de contato para o email, para tal será usado o Nodemailer, módulo para aplicações Node.js que permite enviar email. O lado server-side será responsável por habilitar o servidor, obter as informações submetidas pelo formulário e enviá-las para o email informado na configuração. O lado client-side será responsável por estruturar e estilizar a página do formulário de contato.
+
+### Tarefa
+
+Com base no [mockup](https://www.figma.com/file/Cej98HXCcHfeLmfPQ6cwR1/Interior-Contact-Us?node-id=0%3A1) apresentado, usando HTML, CSS, NodeJS, ExpressJS, Dotenv e Nodemailer construa a página de contato da Interior Design e configure-o para que todas as respostas do formulário sejam enviadas para o seu email.
+
---
+++ index.js
@@ -0,0 +1,70 @@
+
+const express = require('express')
+const nodemailer = require('nodemailer')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+require('dotenv/config');
+
+//parte do bodyparser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+
+app.use(express.static('public'))
+
+
+
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.SENHA
+  }
+})
+
+app.post('/email', (req, res) => {
+
+const email = req.body.email
+const name = req.body.name
+const message = req.body.text
+
+
+
+
+  let mailOptions = {
+    from:`${name} <iuryifba@gmail.com> `,
+    to: 'iuryreivax@hotmail.com',
+    replyTo: `${email}`,
+    subject:'Contact interior',
+    html:
+    ` <h4>Mensagem de ${name}</h4><br>
+      <p>${message}</p><br>
+      <p>Email para contato: ${email}</p>
+    `
+  }
+
+
+  
+
+  transporter.sendMail (mailOptions, function(error,info){
+    if(error){
+      console.log(error)
+    }
+
+    else{
+      console.log('Email sent '+ info.response)
+    }
+
+  })
+
+  
+  res.redirect('/');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
